4aa2adf294884edfe72d7b4cf3c3dbe8c73a9661
Son Tran-Nguyen <github@sntran.com>
2023-09-10 18:25:13 -0500

    feat: `atq` to list current jobs in queue
    If no queue name is passed, lists all jobs; otherwise, list only jobs in
    that queue.
    
    Here, we store the request in datastore under `queue:id` key, and only
    enqueue with the queue and id.
    On `listenQueue`, retrieves the request from datastore to execute, then
    delete it.

--- main.ts
+++ main.ts
@@ -8,22 +8,28 @@ const PREFIX = "jobs";
 const DATABASE_URL = Deno.env.get("DATABASE_URL");
 const db = await Deno.openKv(DATABASE_URL);
 
-db.listenQueue((request: unknown) => {
+db.listenQueue(async (job: unknown) => {
+  const { id, queue } = job as { id: number, queue: string };
+  const key = [PREFIX, `${queue}:${id}`];
+  const { value: request } = await db.get<Request>(key);
+  if (!request) return;
+
   // Reconstructs the serialized request.
-  const { url, ...init} = request as Request;
+  const { url, ...init} = request;
   // Executes the job and closes response body.
   fetch(url, init).then((response) => response.body?.cancel());
+  // Removes the job from the database.
+  await db.delete(key);
 });
 
 /**
- * Parses the time string to timestamp.
+ * Parses the time string to Date.
  *
  * @param {string} time - The time string to parse
- * @returns {number} The timestamp
+ * @returns {Date}
  */
-function parseTime(time: string): number {
-  const timestamp = new Date(time).getTime();
-  return timestamp;
+function parseTime(time: string): Date {
+  return new Date(time);
 }
 
 /**
@@ -42,27 +48,61 @@ export async function at(request: string|URL|Request, time: string, queue = "a")
   request = new Request(request);
   // Serialize the request to be stored in the database.
   const { url, method, headers, body } = request;
-  const job = { url, method, headers, body };
+
+  const date = parseTime(time);
+  const expireIn = date.getTime() - Date.now();
+
+  headers.set("Date", date.toUTCString());
+  const job = { url, method, headers: Object.fromEntries(headers), body };
 
   const idKey = [PREFIX, "size"];
-  let jobId = null;
-  const expireIn = parseTime(time) - Date.now();
+  let id = null;
 
   // Retry the transaction until it succeeds.
   let res = { ok: false };
   while (!res.ok) {
     // Gets the current job id
     const idRes = await db.get<number>(idKey);
-    jobId = Number(idRes.value);
+    id = Number(idRes.value);
+    const jobKey = [PREFIX, `${queue}:${id}`];
 
     res = await db.atomic()
       .check(idRes) // Ensures the current job ID has not changed.
-      .enqueue(job, { delay: expireIn }) // Adds the job to the queue for execution
+      .check({ key: jobKey, versionstamp: null }) // Ensures the job does not exist.
+      .set(jobKey, job, { expireIn }) // Stores the job by its number
+      .enqueue({ id, queue }, { delay: expireIn }) // Adds the job to the queue for execution
       .sum(idKey, 1n) // Increments the job ID
       .commit();
   }
 
-  return jobId;
+  return id;
+}
+
+/**
+ * Lists the user's pending jobs
+ *
+ * If the user is the superuser, everybody's jobs are listed.
+ */
+export async function atq(queue?: string) {
+  const start = [PREFIX];
+  if (queue) {
+    start.push(`${queue}:`);
+  }
+  const entries = await db.list<Request>({
+    prefix: [PREFIX],
+    start,
+  });
+
+  const jobs: Request[] = [];
+
+  for await (const { key, value } of entries) {
+    const [, id] = key;
+    if (id === "size") continue;
+    const { url, ...init } = value;
+    jobs.push(new Request(url, init));
+  }
+
+  return jobs;
 }
 
 // Learn more at https://deno.land/manual/examples/module_metadata#concepts
--- main_test.ts
+++ main_test.ts
@@ -1,5 +1,5 @@
-import { assertEquals, assertNotEquals } from "https://deno.land/std@0.201.0/assert/mod.ts";
-import { at } from "./main.ts";
+import { assert, assertEquals, assertNotEquals } from "https://deno.land/std@0.201.0/assert/mod.ts";
+import { at, atq } from "./main.ts";
 
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
@@ -25,7 +25,7 @@ Deno.test("at", async (t) => {
 
     let request: string|URL|Request, jobId: number | null;
 
-    request = `http://${hostname}:${port}`;
+    request = `http://${hostname}:${port}/`;
     jobId = await at(request, time);
     assertEquals(jobId, 0, "should return the first job id with string request");
 
@@ -51,7 +51,7 @@ Deno.test("at", async (t) => {
     const now = new Date();
     const time = new Date(now.getTime() + 10);
 
-    const request = new Request(`http://${hostname}:${port}`);
+    const request = new Request(`http://${hostname}:${port}/`);
     await at(request, time.toISOString());
 
     assertEquals(
@@ -65,5 +65,49 @@ Deno.test("at", async (t) => {
       lastRequestTimestamp, date,
       "should reflect the timestamp after job is executed"
     );
+  });
+});
+
+Deno.test("atq", async (t) => {
+  await t.step("should list all added jobs when no queue specified", async () => {
+    const now = new Date();
+    const time = new Date(now.getTime() + 10).toISOString();
+    const request = `http://${hostname}:${port}/`;
+
+    await at(request, time);
+
+    const jobs = await atq();
+    assert(jobs.find((job) => job.url === request), "should list the added job");
+  });
+
+  await t.step("should list added jobs in specified queue", async () => {
+    const now = new Date();
+    const time = new Date(now.getTime() + 10).toISOString();
+    const request = `http://${hostname}:${port}/`;
+
+    await at(request, time);
+
+    let jobs = await atq("a");
+    assert(jobs.find((job) => job.url === request), "should list the added job in default queue");
+
+    jobs = await atq("b");
+    assert(!jobs.find((job) => job.url === request), "should not list the added job in other queue");
+
+    await at(request, time, "b");
+    jobs = await atq("b");
+    assert(jobs.find((job) => job.url === request), "should list the added job in correct queue");
+  });
+
+  await t.step("should not list expired job", async () => {
+    const now = new Date();
+    const delay = 10;
+    const time = new Date(now.getTime() + delay).toISOString();
+    const request = `http://${hostname}:${port}/`;
+
+    await at(request, time);
+    await sleep(delay + 10); // wait for job to expire
+
+    const jobs = await atq();
+    assert(!jobs.find((job) => job.url === request), "should not list the expired job in default queue");
   });
 });
