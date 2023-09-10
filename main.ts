d318238e461316676a3fb68a97ea18f1b0959e4b
Son Tran-Nguyen <github@sntran.com>
2023-09-10 11:54:46 -0500

    feat: schedule request to fetch in future
    Use `enqueue` and `listenQueue` for scheduling.
    
    Each job in the queue has a number starting from 0.
    
    Enqueued `Request` is sent with `fetch` after specified time.

--- main.ts
+++ main.ts
@@ -1,8 +1,71 @@
-export function add(a: number, b: number): number {
-  return a + b;
+/**
+ * Schedules commands to be executed once, at a particular time in the future.
+ *
+ * The commands are executed at a later time, using `fetch`.
+ */
+
+const PREFIX = "jobs";
+const DATABASE_URL = Deno.env.get("DATABASE_URL");
+const db = await Deno.openKv(DATABASE_URL);
+
+db.listenQueue((request: unknown) => {
+  // Reconstructs the serialized request.
+  const { url, ...init} = request as Request;
+  // Executes the job and closes response body.
+  fetch(url, init).then((response) => response.body?.cancel());
+});
+
+/**
+ * Parses the time string to timestamp.
+ *
+ * @param {string} time - The time string to parse
+ * @returns {number} The timestamp
+ */
+function parseTime(time: string): number {
+  const timestamp = new Date(time).getTime();
+  return timestamp;
+}
+
+/**
+ * Adds a job to be executed at specific time.
+ *
+ * Each jobs will have an auto-incremented number as ID in the job queue.
+ * Note: Job number starts at 0.
+ *
+ * @param {string|URL|Request} request - The request to be executed
+ * @returns {number} - The job number
+ */
+export async function at(request: string|URL|Request, time: string, queue = "a"): Promise<number | null> {
+  if (typeof request === "string") {
+    request = new URL(request);
+  }
+  request = new Request(request);
+  // Serialize the request to be stored in the database.
+  const { url, method, headers, body } = request;
+  const job = { url, method, headers, body };
+
+  const idKey = [PREFIX, "size"];
+  let jobId = null;
+  const expireIn = parseTime(time) - Date.now();
+
+  // Retry the transaction until it succeeds.
+  let res = { ok: false };
+  while (!res.ok) {
+    // Gets the current job id
+    const idRes = await db.get<number>(idKey);
+    jobId = Number(idRes.value);
+
+    res = await db.atomic()
+      .check(idRes) // Ensures the current job ID has not changed.
+      .enqueue(job, { delay: expireIn }) // Adds the job to the queue for execution
+      .sum(idKey, 1n) // Increments the job ID
+      .commit();
+  }
+
+  return jobId;
 }
 
 // Learn more at https://deno.land/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+
 }
--- main_test.ts
+++ main_test.ts
@@ -1,6 +1,69 @@
-import { assertEquals } from "https://deno.land/std@0.201.0/assert/mod.ts";
-import { add } from "./main.ts";
+import { assertEquals, assertNotEquals } from "https://deno.land/std@0.201.0/assert/mod.ts";
+import { at } from "./main.ts";
 
-Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+type Address = { hostname: string; port: number };
+
+let lastRequestTimestamp: number;
+
+// A simple server that updates last request's timestamp.
+const { hostname, port } = await new Promise<Address>((onListen) => {
+  Deno.serve({
+    port: 0,
+    onListen,
+  }, (_request: Request) => {
+    lastRequestTimestamp = Date.now();
+    return new Response("ok");
+  });
+});
+
+Deno.test("at", async (t) => {
+  await t.step("should add jobs with auto-increased number", async () => {
+    const now = new Date();
+    const time = new Date(now.getTime() + 10).toISOString();
+
+    let request: string|URL|Request, jobId: number | null;
+
+    request = `http://${hostname}:${port}`;
+    jobId = await at(request, time);
+    assertEquals(jobId, 0, "should return the first job id with string request");
+
+    request = new URL(request);
+    jobId = await at(request, time);
+    assertEquals(jobId, 1, "should return the second job id with URL request");
+
+    request = new Request(request);
+    jobId = await at(request, time);
+    assertEquals(jobId, 2, "should return the third job id with Request request");
+
+    const jobIds = [3, 4, 5, 6, 7, 8, 9, 10]
+    assertEquals(
+      await Promise.all(jobIds.map(() => at(request, time))),
+      jobIds,
+      "should return the next job ids in correct sequence"
+    );
+  });
+
+  await t.step("should execute jobs at specified time", async () => {
+    const date = lastRequestTimestamp;
+
+    const now = new Date();
+    const time = new Date(now.getTime() + 10);
+
+    const request = new Request(`http://${hostname}:${port}`);
+    await at(request, time.toISOString());
+
+    assertEquals(
+      lastRequestTimestamp, date,
+      "should not change last timestamp before job is executed"
+    );
+
+    await sleep(time.getTime() - now.getTime());
+
+    assertNotEquals(
+      lastRequestTimestamp, date,
+      "should reflect the timestamp after job is executed"
+    );
+  });
 });
